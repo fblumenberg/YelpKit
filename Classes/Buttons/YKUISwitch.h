234c9dddc51cfcf9b1137143652da0a44da3435b
John Boiles <johnb@yelp.com>
2012-12-06 15:57:48 -0800

    Adding one bit to YKUISwitch protocol

--- Classes/Buttons/YKUISwitch.h
+++ Classes/Buttons/YKUISwitch.h
@@ -8,6 +8,18 @@
 
 @protocol YKUISwitch <NSObject>
 - (void)setOn:(BOOL)on animated:(BOOL)animated;
+
+/*!
+ Set the state of the button. When ignoreControlEvents is YES, the button won't call any registered
+ target for UIControlEventValueChanged. This is useful for setting the button programatically without
+ executing code that should only happen when the switch is changed from a user event.
+
+ @param newOn Button state
+ @param animated Whether to animate the transition
+ @param ignoreControlEvents If YES, any target set for UIControlEventValueChanged will not be called
+*/
+- (void)setOn:(BOOL)on animated:(BOOL)animated ignoreControlEvents:(BOOL)ignoreControlEvents;
+
 - (BOOL)isOn;
 @end
 
